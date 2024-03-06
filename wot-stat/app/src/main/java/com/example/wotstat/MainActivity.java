f74175cedaac4b4031b43a9409a15ec6fe19b9e2
KpoJleBapKa <ars05@ukr.net>
2024-03-06 15:43:23 +0200

    v0.3 alerts

--- wot-stat/app/src/main/java/com/example/wotstat/MainActivity.java
+++ wot-stat/app/src/main/java/com/example/wotstat/MainActivity.java
@@ -5,34 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView resultTextView;
-    EditText number_field_1, number_field_2;
-    Button find_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        resultTextView = findViewById(R.id.resultTextView);
-        number_field_1 = findViewById(R.id.number_field_1);
-        number_field_2 = findViewById(R.id.number_field_2);
-        find_button = findViewById(R.id.find_button);
+    }
 
-        find_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                float num1 = Float.parseFloat(number_field_1.getText().toString());
-                float num2 = Float.parseFloat(number_field_2.getText().toString());
-                float res = num1 + num2;
-                resultTextView.setText(String.valueOf(res));
-            }
-        });
+    public void btnClick(View v) {
+        showInfo(((Button) v).getText().toString());
+    }
 
+    private void showInfo(String text) {
+        Toast.makeText(this, "niggers", Toast.LENGTH_SHORT).show();
     }
 }
