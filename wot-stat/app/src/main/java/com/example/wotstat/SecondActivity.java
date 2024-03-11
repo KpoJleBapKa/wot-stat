2da4a94f48d516123e852bdcf49caabcaea1e823
KpoJleBapKa <ars05@ukr.net>
2024-03-11 18:58:29 +0200

    v0.4  go-back buttons

--- wot-stat/app/src/main/java/com/example/wotstat/SecondActivity.java
+++ wot-stat/app/src/main/java/com/example/wotstat/SecondActivity.java
@@ -5,20 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
     }
 
-    public void startNewActivity (View v) {
-        Intent intent = new Intent(this, SecondActivity.class);
+    public void goBack(View v) {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
